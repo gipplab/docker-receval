84bc20014bed97afd85d166177ea6b7e34a74c0f
malteos <7977470+malteos@users.noreply.github.com>
2020-04-15 14:28:14 +0200

    Updated admin views

--- receval/apps/explorer/admin.py
+++ receval/apps/explorer/admin.py
@@ -4,6 +4,30 @@ from receval.apps.explorer.models import RecommendationSet, Recommendation, Item
 
 admin.site.register(RecommendationSet)
 admin.site.register(Recommendation)
-admin.site.register(Item)
-admin.site.register(Experiment)
-admin.site.register(Feedback)
+
+
+@admin.register(Experiment)
+class ExperimentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'external_id', 'experiment')
+    list_filter = ('experiment__name', )
+    actions = []
+    list_select_related = ('experiment', )
+    # autocomplete_fields = ['experiment']
+    search_fields = ['experiment']
+    exclude = []
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'recommendation', 'is_relevant', 'comment', 'updated_date')
+    # list_filter = ('experiment__name', )
+    actions = []
+    # list_select_related = ('experiment', )
+    # autocomplete_fields = ['experiment']
+    # search_fields = ['experiment']
+    exclude = []
